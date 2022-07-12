b686336505885808fa42c7d2fa0cdae3a90f77d1
Chris <wiiqwertyuiop@gmail.com>
2022-07-12 12:22:23 -0400

    Added registration page

--- src/main/java/com/example/atzfinance/Service/LoginService.java
+++ src/main/java/com/example/atzfinance/Service/LoginService.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.example.atzfinance.Model.User;
