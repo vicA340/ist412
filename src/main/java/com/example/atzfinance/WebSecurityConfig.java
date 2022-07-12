ce0695d6069acc0a3fb2b93518c06324bfd11a15
Chris <wiiqwertyuiop@gmail.com>
2022-07-12 14:41:45 -0400

    Added logout

--- src/main/java/com/example/atzfinance/WebSecurityConfig.java
+++ src/main/java/com/example/atzfinance/WebSecurityConfig.java
@@ -10,7 +10,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
  
 @Configuration
 public class WebSecurityConfig {
