3af2969aa37ab7435a2307a9e63e8a64557c25d1
Chris <wiiqwertyuiop@gmail.com>
2022-07-26 17:03:48 -0400

    getIsUnderwriter

--- src/main/java/com/example/atzfinance/Controller/Dashboard/DashboardController.java
+++ src/main/java/com/example/atzfinance/Controller/Dashboard/DashboardController.java
@@ -2,28 +2,28 @@ package com.example.atzfinance.Controller.Dashboard;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.example.atzfinance.Model.User;
 import com.example.atzfinance.Service.LoanService;
+import com.example.atzfinance.Service.LoginService;
 
 @Controller
 public class DashboardController {
     
     @Autowired
+    LoginService userDetailsService;
+    
+    @Autowired
     private LoanService loanService;
     
     @GetMapping("/dashboard")
-    public String dashboard(Model model, Authentication authentication) {
+    public String dashboard(Model model, Authentication authentication) {    
+        User user = userDetailsService.loadUserByUsername(authentication.getName());
+        model.addAttribute("isUnderwriter", user.getIsUnderwriter());
         model.addAttribute("listLoans", loanService.getAllLoans());
-    
-        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-
-        model.addAttribute("isUnderwriter", user);
         return "Dashboard/dashboard";
     }
 }
