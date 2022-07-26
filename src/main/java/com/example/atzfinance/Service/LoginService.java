38c71ac31eb771e4859cdfc319f742990c36f779
Chris <wiiqwertyuiop@gmail.com>
2022-07-26 17:04:18 -0400

    WIP

--- src/main/java/com/example/atzfinance/Service/LoginService.java
+++ src/main/java/com/example/atzfinance/Service/LoginService.java
@@ -1,7 +1,6 @@
 package com.example.atzfinance.Service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
