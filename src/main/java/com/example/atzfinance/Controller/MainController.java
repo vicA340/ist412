71fe0edf2c42fc6acb815e77fbe98ce9b0ed2267
Chris <wiiqwertyuiop@gmail.com>
2022-07-26 14:44:12 -0400

    Fixed conflict

--- src/main/java/com/example/atzfinance/Controller/MainController.java
+++ src/main/java/com/example/atzfinance/Controller/MainController.java
@@ -1,16 +1,10 @@
 package com.example.atzfinance.Controller;
 
-import com.example.atzfinance.Service.LoanService;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
  
 @Controller
 public class MainController {
-     
-    @Autowired
-    private LoanService loanService;
 
     @GetMapping("/")
     public String viewHomePage() {
