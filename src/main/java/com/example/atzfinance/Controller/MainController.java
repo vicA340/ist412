e5b7924d85e6ceee1b6a668bea223ae6716064b1
Chris <wiiqwertyuiop@gmail.com>
2022-07-26 14:43:37 -0400

    Fixed conflict

--- src/main/java/com/example/atzfinance/Controller/MainController.java
+++ src/main/java/com/example/atzfinance/Controller/MainController.java
@@ -4,7 +4,6 @@ import com.example.atzfinance.Service.LoanService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
  
 @Controller
@@ -16,13 +15,6 @@ public class MainController {
     @GetMapping("/")
     public String viewHomePage() {
         return "index";
-    }
-
-    @GetMapping("/dashboard")
-    public String dashboard(Model model) {
-        model.addAttribute("listLoans", loanService.getAllLoans());
-        
-        return "dashboard";
     }
 
     @GetMapping("/login")
