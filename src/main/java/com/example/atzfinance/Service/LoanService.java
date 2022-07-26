de89594e3e78a34f9722cc16d6d962134be0c08f
Chris <wiiqwertyuiop@gmail.com>
2022-07-26 14:52:21 -0400

    Fixed conflicts

--- src/main/java/com/example/atzfinance/Service/LoanService.java
+++ src/main/java/com/example/atzfinance/Service/LoanService.java
@@ -1,14 +1,50 @@
 package com.example.atzfinance.Service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.atzfinance.Repository.LoanRepository;
 import com.example.atzfinance.Model.Loan;
+import com.example.atzfinance.Model.Loan.LoanStatus;
 
 import java.util.List;
+import java.util.Optional;
 
-public interface LoanService {
-    List<Loan> getAllLoans();
-    void saveLoan(Loan loan);
-    Loan getLoanById(long id);
-    void deleteLoanById(long id);
-    void denyLoan(long id);
-    void approveLoan(long id);
+@Service
+public class LoanService {
+
+    @Autowired
+    private LoanRepository loanRepository;
+
+    public List<Loan> getAllLoans() {
+        return this.loanRepository.findAll();
+    }
+
+    public void saveLoan(Loan loan) {
+        this.loanRepository.save(loan);
+    }
+
+    public Loan getLoanById(long id) throws RuntimeException {
+        Optional<Loan> optional = loanRepository.findById(id);
+        if (!optional.isPresent()) {
+            throw new RuntimeException("Loan not found for id :: " + id);
+        }
+        return optional.get();
+    }
+
+    public void deleteLoanById(long id) {
+        this.loanRepository.deleteById(id);
+    }
+    
+    public void denyLoan(long id) {
+        Loan loan = getLoanById(id);
+        loan.setStatus(LoanStatus.DENIED);
+        saveLoan(loan);
+    }
+
+    public void approveLoan(long id) {
+        Loan loan = getLoanById(id);
+        loan.setStatus(LoanStatus.APPROVED);
+        saveLoan(loan);
+    }
 }
